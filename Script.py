b10b5446e5a7a5f21d214fb0a8e2070ff502f2e3
ChampATW <47860671+ChampATW@users.noreply.github.com>
2021-04-04 16:43:29 -0400

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,3 +6,16 @@ def wget (url):
 wget("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts")
 wget("https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/MalwareDomainList.com/master/domains.list")
 
+
+#Parse The StevenBlack Host file
+file1= "hosts"
+file2="domains.list"
+
+openfile = open(file1, 'r')
+Lines = openfile.readlines()
+count = 0
+for line in Lines:
+    count += 1
+    if count > 39 and count < 139: #The blacklisted domains start at line 39 hence why we are ignoring lines until this point
+        baddomain = line.replace('0.0.0.0','')
+        print(baddomain)
