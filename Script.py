3acbda98c3f0572cd6ce24a05b119dcd307b67e9
ChampATW <47860671+ChampATW@users.noreply.github.com>
2021-04-04 16:49:34 -0400

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,15 +9,24 @@ wget("https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/MalwareDomainLi
 Domainlist = []
 #Parse The StevenBlack Host file
 file1= "hosts"
-file2="domains.list"
-
 openfile = open(file1, 'r')
 Lines = openfile.readlines()
 count = 0
 for line in Lines:
     count += 1
     if count > 39 and count < 139: #The blacklisted domains start at line 39 hence why we are ignoring lines until this point
-        baddomain = line.replace('0.0.0.0','')
-        print(baddomain)
+        baddomain = line.replace('0.0.0.0','') #Remove the unneeded data from the string
         Domainlist.append(baddomain)
-print (baddomain)
+#Finished StevenBlack next is adding 100 URLs from the other file
+
+#Adding 100 URLs from the other list to baddomains
+file2="domains.list"
+openfile = open(file2, 'r')
+Lines = openfile.readlines()
+count=0
+for line in Lines:
+    count += 1
+    if count > 0 and count < 100:
+        Domainlist.append(line)
+
+print (Domainlist)
