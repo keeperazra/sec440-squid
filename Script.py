e42511776da90e890968e996df14681fc3666ac1
ChampATW <47860671+ChampATW@users.noreply.github.com>
2021-04-04 16:34:00 -0400

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,3 +4,5 @@ def wget (url):
     location = os.getcwd()
     os.system("wget "+ url+ " -P"+location )
 wget("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts")
+wget("https://raw.githubusercontent.com/Ultimate-Hosts-Blacklist/MalwareDomainList.com/master/domains.list")
+
